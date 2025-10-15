ad5f1c48114153de04e457e8aead98b855da306e
ReeshaMartis <reeshamartis@gmail.com>
2025-10-15 16:35:27 -0400

    planet_service added with CRUD methods

---
+++ app/services/planet_service.rb
@@ -0,0 +1,65 @@
+class PlanetService
+    def index
+        planets = Planet.all
+        if planets.any?
+            planets
+        else
+            "no planets in this galaxy"
+        end
+    end
+
+    def create
+        planet_param = params.require(:planet).permit(:name,:position)
+        if Planet.exists?(name: planet_param[:name])
+            return "Planet already exists"
+        else
+            planet = Planet.new(planet_param)
+
+            if Planet.savein
+                planet
+            else
+                "Error in creating this planet"
+            end
+        end
+    end
+
+    def create_bulk
+        res =[]
+        planets_data = params.require(:planet)
+        planets_data.each do |planet_param|
+            if Planet.exists?(name: planet_param[:name])
+                res << "Planet #{planet_param[:name]} exists already"
+            else
+                new_planet = Planet.new(planet_param.permit(:name,:position))
+                if new_planet.save
+                    res << "Planet #{planet_param[:name]} created successfully"
+                else
+                    res << "Planet #{planet_param[:name]} creation failed"
+                end
+            end  
+        end    
+        res
+    end
+
+    def update
+        planet_param = params.require(:planet).permit(:name,:position)
+        upd_planet = Planet.find_by(name: planet_param[:name])
+        if upd_planet
+            upd_planet.update(planet_param)
+            upd_planet
+        else
+            "No such planet exists in this galaxy, Try again?"
+        end
+    end
+
+    def delete
+        planet_param = params.require(:planet).permit(:name,:position)
+        del_planet = Planet.find_by(name: planet_para[:name])
+        if del_planet
+            del_planet.destroy
+        else
+            "No such planet exists in this galaxy, Try again?"
+        end
+    end
+    
+end
